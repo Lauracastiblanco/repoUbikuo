6bfe5cb148cf3412fc1d9b19b0314e2b1f38246a
Diego Berdugo <104031707+BERDUGO93@users.noreply.github.com>
2023-05-26 16:02:04 -0500

    Yeslhan
    Validación del mail para recuperar contraseña, aún falta una función en javaScript

--- src/java/JavaMail/envioCorreo.java
+++ src/java/JavaMail/envioCorreo.java
@@ -8,12 +8,18 @@ package JavaMail;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import static jdk.nashorn.internal.objects.NativeFunction.function;
 
 /**
  *
@@ -43,17 +49,55 @@ public class envioCorreo extends HttpServlet {
 
         String resultadoMensaje = "";
 
-        try {
-            PropiedadesCorreo.envioCorreo(servidor, puerto, usuario, clave, destino, asunto, mensaje);
-            resultadoMensaje = "El correo ha sido enviado.";
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            resultadoMensaje = "Error al enviar el correo: " + e.getMessage();
-        } finally {
-            request.setAttribute("EstadoMensaje", resultadoMensaje);
-            getServletContext().getRequestDispatcher("/mensajeConfirmacion.jsp").forward(request, response);
+        if (validarCorreo(destino)) {
+            try {
+                PropiedadesCorreo.envioCorreo(servidor, puerto, usuario, clave, destino, asunto, mensaje);
+                resultadoMensaje = "El correo ha sido enviado.";
+            } catch (Exception e) {
+                e.printStackTrace();
+                resultadoMensaje = "Error al enviar el correo: " + e.getMessage();
+            }
+        } else {
+            resultadoMensaje = "El correo electrónico no existe en la base de datos.";
         }
-    }
 
+        request.setAttribute("EstadoMensaje", resultadoMensaje);
+        getServletContext().getRequestDispatcher("/mensajeConfirmacion.jsp").forward(request, response);
+    }
+    
+    private boolean validarCorreo(String correo){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ubikuo_final", "root", "");
+            String sql = "SELECT COUNT(*) FROM tblusuarios WHERE usuemail = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, correo);
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                return count > 0;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
 }
+
